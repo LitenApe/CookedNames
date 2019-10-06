4e42144402c6fbaa277e4719c4e9af55ebad92f6
LitenApe <sonvo95@outlook.com>
2019-10-06 11:54:15 +0200

    fix: Prevent namespace collisions
    Wrap logic inside an anonymous function to prevent namespace collisions.

--- index.js
+++ index.js
@@ -1,53 +1,55 @@
-/**
- * Utility function to concatenate css classnames in a
- * tidy and simple fashion. Takes in an arbitraty number
- * of arguments and returns all truthy arguments as an
- * css class string.
- * @returns {string | undefined} Returns a css valid combination of classnames
- */
-function cookedNames() {
-  "use strict"
+(function() {
   /**
-   * Retrieves and returns lower cased string of the object type.
-   * @param {*} obj 
-   * @returns {string} string | number | boolean | object | array | arguments | function | error | date | regexp | undefined | null
+   * Utility function to concatenate css classnames in a
+   * tidy and simple fashion. Takes in an arbitraty number
+   * of arguments and returns all truthy arguments as an
+   * css class string.
+   * @returns {string | undefined} Returns a css valid combination of classnames
    */
-  function getType(obj) {
-    const objectType = Object.prototype.toString.call(obj);
-    return objectType.match(/\[\w+ (\w+)\]/)[1].toLowerCase();
-  }
-
-  let classes = "";
-
-  for (let i = 0; i < arguments.length; i++) {
-    const arg = arguments[i];
-    
-    if (!arg) continue;
-    
-    const type = getType(arg);
-
-    if (type === "string" || type === "number") {
-      classes += `${arg} `;
-    } else if (type === "object") {
-      for (let key in arg) {
-        if (arg[key]) classes += `${key} `;
-      }
-    } else if (type === "array") {
-      const deep = cookedNames.apply(null, arg);
-      if (deep) classes += `${deep} `;
+  function cookedNames() {
+    "use strict"
+    /**
+     * Retrieves and returns lower cased string of the object type.
+     * @param {*} obj 
+     * @returns {string} string | number | boolean | object | array | arguments | function | error | date | regexp | undefined | null
+     */
+    function getType(obj) {
+      const objectType = Object.prototype.toString.call(obj);
+      return objectType.match(/\[\w+ (\w+)\]/)[1].toLowerCase();
     }
+    
+    let classes = "";
+    
+    for (let i = 0; i < arguments.length; i++) {
+      const arg = arguments[i];
+      
+      if (!arg) continue;
+      
+      const type = getType(arg);
+      
+      if (type === "string" || type === "number") {
+        classes += `${arg} `;
+      } else if (type === "object") {
+        for (let key in arg) {
+          if (arg[key]) classes += `${key} `;
+        }
+      } else if (type === "array") {
+        const deep = cookedNames.apply(null, arg);
+        if (deep) classes += `${deep} `;
+      }
+    }
+    
+    if (!classes) return;
+    
+    return classes.trimRight();
   }
-
-  if (!classes) return;
-
-  return classes.trimRight();
-}
-
-if (module && module.exports) {
-  cookedNames.default = cookedNames;
-  module.exports = cookedNames;
-} else if (define && define.amd) {
-  define("cookednames", [], () => cookedNames);
-} else {
-  window.cookedNames = cookedNames;
-}
+  
+  if (module && module.exports) {
+    cookedNames.default = cookedNames;
+    module.exports = cookedNames;
+  } else if (define && define.amd) {
+    define("cookednames", [], () => cookedNames);
+  } else {
+    window.cookedNames = cookedNames;
+  }
+}());
