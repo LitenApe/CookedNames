8b3ffac9673fc901bd16589906053caf8e8067f0
LitenApe <sonvo95@outlook.com>
2019-10-06 14:00:07 +0200

    perf: Use native operations for type checking
    Use native methods such as typeof and Array.isArray to retrieve the argument type instead of the string/regex combination used earlier to increase the overall performance of the utility function.
    
    Additionally, switched to direct recursive function calls instead if apply, as the direct method seems to be alot more performant on newer browsers compared to both .apply and .call.

--- index.js
+++ index.js
@@ -8,42 +8,32 @@
    */
   function cookedNames() {
     "use strict"
-    /**
-     * Retrieves and returns lower cased string of the object type.
-     * @param {*} obj 
-     * @returns {string} string | number | boolean | object | array | arguments | function | error | date | regexp | undefined | null
-     */
-    function getType(obj) {
-      const objectType = Object.prototype.toString.call(obj);
-      return objectType.match(/\[\w+ (\w+)\]/)[1].toLowerCase();
-    }
-    
     let classes = "";
-    
+
     for (let i = 0; i < arguments.length; i++) {
       const arg = arguments[i];
-      
+
       if (!arg) continue;
-      
-      const type = getType(arg);
-      
+
+      const type = typeof arg;
+
       if (type === "string" || type === "number") {
         classes += `${arg} `;
+      } else if (Array.isArray(arg)) {
+        const deep = cookedNames(...arg);
+        if (deep) classes += `${deep} `;
       } else if (type === "object") {
         for (let key in arg) {
           if (arg[key]) classes += `${key} `;
         }
-      } else if (type === "array") {
-        const deep = cookedNames.apply(null, arg);
-        if (deep) classes += `${deep} `;
       }
     }
-    
+
     if (!classes) return;
-    
+
     return classes.trimRight();
   }
-  
+
   if (module && module.exports) {
     cookedNames.default = cookedNames;
     module.exports = cookedNames;
